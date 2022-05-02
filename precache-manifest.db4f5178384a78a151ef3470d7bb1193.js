604b541553a9974ad7d66a6c327545b005b96e8e
Vincenzofdg <vfdgiacomo@gmail.com>
2022-05-02 02:03:03 -0300

    Updates

---
+++ precache-manifest.db4f5178384a78a151ef3470d7bb1193.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8614c99ace65513a31d7efeb1848fa9a",
+    "url": "/star-wars-planets/index.html"
+  },
+  {
+    "revision": "43383d7a1ce913225e3a",
+    "url": "/star-wars-planets/static/css/main.26d320cc.chunk.css"
+  },
+  {
+    "revision": "5c0180202ae138982fba",
+    "url": "/star-wars-planets/static/js/2.a46755cf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/star-wars-planets/static/js/2.a46755cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43383d7a1ce913225e3a",
+    "url": "/star-wars-planets/static/js/main.d7ecbfcc.chunk.js"
+  },
+  {
+    "revision": "6b480a4a8b5688e80087",
+    "url": "/star-wars-planets/static/js/runtime-main.6f08aa3c.js"
+  },
+  {
+    "revision": "0c1540f1a0e5c4df85dbe5aedc9a3773",
+    "url": "/star-wars-planets/static/media/background.0c1540f1.jpg"
+  }
+]);
